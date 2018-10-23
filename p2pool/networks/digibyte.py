6436903378339543512acb9e0d8f5a0ea1b844d3
farsider350 <31876349+farsider350@users.noreply.github.com>
2018-10-23 20:42:56 +1000

    Updated bootstrap

--- p2pool/networks/digibyte.py
+++ p2pool/networks/digibyte.py
@@ -1,20 +1,20 @@
 from p2pool.bitcoin import networks
 
 PARENT = networks.nets['digibyte']
-SHARE_PERIOD=25
-CHAIN_LENGTH=24*60*60//10
-REAL_CHAIN_LENGTH=24*60*60//10
-TARGET_LOOKBEHIND=200
-SPREAD=30
-IDENTIFIER='1c017dc97693f7d5'.decode('hex')
-PREFIX='1c017dc977c524d6'.decode('hex')
-P2P_PORT=5026
-MIN_TARGET=0
-MAX_TARGET=2**256//2**20 - 1
-PERSIST=True
-WORKER_PORT=5027
-BOOTSTRAP_ADDRS='crypto.office-on-the.net p2p-spb.xyz 46.188.44.20 siberia.mine.nu gigablock.mine.nu tomsk.mine.nu 45.32.210.32'.split(' ')
-ANNOUNCE_CHANNEL='#p2pool'
+SHARE_PERIOD = 25
+CHAIN_LENGTH = 24*60*60//10
+REAL_CHAIN_LENGTH = 24*60*60//10
+TARGET_LOOKBEHIND = 200
+SPREAD = 30
+IDENTIFIER = '1c017dc97693f7d5'.decode('hex')
+PREFIX = '1c017dc977c524d6'.decode('hex')
+P2P_PORT = 5026
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**20 - 1
+PERSIST = True
+WORKER_PORT = 5027
+BOOTSTRAP_ADDRS = 'crypto.office-on-the.net p2p-spb.xyz 46.188.44.20 siberia.mine.nu gigablock.mine.nu tomsk.mine.nu 45.32.210.32 triplezen.tk'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool'
 VERSION_CHECK = lambda v: None if 6160500 <= v else 'DigiByte version too old. Upgrade to 6.16.5 or newer!'
 VERSION_WARNING = lambda v: None
 SOFTFORKS_REQUIRED = set(['nversionbips', 'csv', 'segwit'])
